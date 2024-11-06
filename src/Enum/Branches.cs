bc431eafd1757aa58154f0480e7b6e01a32734f0
n4zi8 <nazirul.fatta@gmail.com>
2024-11-06 15:58:19 +0700

    Initial Commit
    Initial Commit

---
+++ src/Enum/Branches.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace src.Enum
+{
+    public enum Branches
+    {
+        [Display(Name = "Head Office")]
+        branch0 = 0,
+
+        [Display(Name = "Bali")]
+        branch1 = 1,
+
+        [Display(Name = "Bandung")]
+        branch2 = 2,
+
+        [Display(Name = "Banjarmasin")]
+        branch3 = 3,
+
+        [Display(Name = "Gading Serpong")]
+        branch4 = 4,
+
+        [Display(Name = "Jakarta Institutional Division")]
+        branch5 = 5,
+
+        [Display(Name = "Jakarta Strategic Division")]
+        branch6 = 6,
+
+        [Display(Name = "Jambi")]
+        branch7 = 7,
+
+        [Display(Name = "Kelapa Gading")]
+        branch8 = 8,
+
+        [Display(Name = "Makassar")]
+        branch9 = 9,
+
+        [Display(Name = "Malang")]
+        branch10 = 10,
+
+        [Display(Name = "Manado")]
+        branch11 = 11,
+
+        [Display(Name = "Medan")]
+        branch12 = 12,
+
+        [Display(Name = "Padang")]
+        branch13 = 13,
+
+        [Display(Name = "Palembang")]
+        branch14 = 14,
+
+        [Display(Name = "Pantai Indah Kapuk")]
+        branch15 = 15,
+
+        [Display(Name = "Pekanbaru")]
+        branch16 = 16,
+
+        [Display(Name = "Pluit")]
+        branch17 = 17,
+
+        [Display(Name = "Pontianak")]
+        branch18 = 18,
+
+        [Display(Name = "Puri Indah")]
+        branch19 = 19,
+
+        [Display(Name = "Semarang")]
+        branch20 = 20,
+
+        [Display(Name = "Solo")]
+        branch21 = 21,
+
+        [Display(Name = "Sudirman")]
+        branch22 = 22,
+
+        [Display(Name = "Surabaya")]
+        branch23 = 23,
+
+        [Display(Name = "Yogyakarta")]
+        branch24 = 24
+    }
+}
---
+++ src/Query/dbo.Customer.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [customerId]     UNIQUEIDENTIFIER NOT NULL,
+    [CreateAt]       DATETIME2 (7)    NOT NULL,
+    [CreateBy]       NVARCHAR (MAX)   NULL,
+    [address]        NVARCHAR (100)   NULL,
+    [customerName]   NVARCHAR (100)   NOT NULL,
+    [customerType]   INT              NOT NULL,
+    [description]    NVARCHAR (200)   NULL,
+    [email]          NVARCHAR (100)   NULL,
+    [branches]       INT   NOT NULL,
+    [organizationId] UNIQUEIDENTIFIER NOT NULL,
+    [phone]          NVARCHAR (20)    NULL,
+    [thumbUrl]       NVARCHAR (255)   NULL,
+    [website]        NVARCHAR (100)   NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([customerId] ASC),
+    CONSTRAINT [FK_Customer_Organization_organizationId] FOREIGN KEY ([organizationId]) REFERENCES [dbo].[Organization] ([organizationId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Customer_organizationId]
+    ON [dbo].[Customer]([organizationId] ASC);
+
